f91443665d8c69560c36f434071dfee67ee6467a
olie-t <olthomas13@gmail.com>
2024-08-21 14:36:39 +0100

    adding sprites

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode(size=(SCREEN_WIDTH, SCREEN_HEIGHT))
     print("Starting asteroids!")
+    clock =  pygame.time.Clock()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
@@ -12,6 +14,8 @@ def main():
                 return
         screen.fill(000000)
         pygame.display.flip()
+        clock_return = clock.tick(60)
+        dt = float(clock_return) / 1000
 
 
 if __name__ == "__main__":
